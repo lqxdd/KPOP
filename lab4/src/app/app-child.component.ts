ca83c3cf9cd08a1ae03e2c976be83434e817d557
Lydia Gavrilyan <59844887+lqxdd@users.noreply.github.com>
2021-10-30 15:47:30 +0300

    Update app-child.component.ts

--- lab4/src/app/app-child.component.ts
+++ lab4/src/app/app-child.component.ts
@@ -18,39 +18,8 @@ export class MyComponent implements OnChanges {
     this.max = Math.max(...this.someInput);
     console.log(this.someInput);
 
-   /*  this.proc(this.textInput);
-
-    this.glebberOtveti = this.quadraticEquation(this.glebber[0], this.glebber[1], this.glebber[2])
-    // @ts-ignore
-    this.answer =  this.glebberOtveti ? `D: ${this.glebberOtveti.discriminant} R1: ${this.glebberOtveti.quadratic_roots[0]} R2: ${this.glebberOtveti.quadratic_roots[1]}`: "false" */
    
   }
 
-  /* proc(text: string) {
-    this.procText =
-      text.replace(/о/g, '?') + ' string length is: ' + text.length;
-  }
-
-  glebberOtveti:  { discriminant: number; quadratic_roots: number | number[]; } | false = false
-  answer:string=""
-
-  quadraticEquation = (a: number, b: number, c: number) => {
-    if (a == 0) return false;
-    let res: { discriminant: number; quadratic_roots: number | number[] } = {
-      discriminant: 0,
-      quadratic_roots: 0,
-    };
-    let D = b * b - 4 * a * c;
-    console.log('D = ' + D);
-    if (D < 0) return false;
-    res['discriminant'] = D;
-    if (D == 0) res['quadratic_roots'] = (-b + Math.sqrt(D)) / (2 * a);
-    else if (D > 0) {
-      let tmp = [];
-      tmp.push((-b + Math.sqrt(D)) / (2 * a));
-      tmp.push((-b - Math.sqrt(D)) / (2 * a));
-      res['quadratic_roots'] = tmp;
-    }
-    return res;
-  }; */
+ 
 }
